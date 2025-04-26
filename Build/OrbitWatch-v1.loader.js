d1ffba471c104e72b8aeaef7a85156889f1432e8
Madeleine Woodbury <mail@woodburycode.com>
2025-04-26 11:47:54 -0400

    feat: replace old Unity WebGL build files with new versions

--- Build/OrbitWatch-v1.loader.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:02e605f58577a7250bea438acc1513ea7f820591151580abfc83943d48127bd1
-size 117327
---
+++ Build/webgl-dist.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4635293f105da6e596c6497a21c9f325fe83b22f5bc7422a0758d7ff01b46a45
+size 117327
